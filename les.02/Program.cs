20afe6052851d67a4dc8beb32074ea1dc196bd4e
mikhail <trushakin@list.ru>
2023-12-13 14:16:28 +0300

    задача из лекции n2

---
+++ les.02/Program.cs
@@ -0,0 +1,31 @@
+﻿//  напишите программу, которая принимает на вход трехзначное 
+// число и возводит вторую цифру этого числа в степень,
+//равную третьей цифре.
+// Примеры 
+// 487=> 8^7 = 2 097 152
+// 254 => 5^4 = 625
+// 617 => 1
+
+Console.WriteLine("Введите число"); 
+int num=Convert.ToInt32(Console.ReadLine());
+
+
+if(num>99 && num<1000) 
+{
+int num2=num/10 %10;  // чисо/10 и остаток от 10
+int num3= num%10; 
+int result =1;
+
+while(num3>=1)
+{
+    result= result *num2;
+    num3--; // num3=num3-1;
+}
+    Console.WriteLine(result);
+
+
+}
+else
+{
+    Console.WriteLine("не верное число!");
+}
