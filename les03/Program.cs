d042c4e0fbfb7ec5052f8d1e030220e0b91c847c
mikhail <trushakin@list.ru>
2023-12-13 15:35:08 +0300

    задача из лекции n3

---
+++ les03/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.WriteLine("Введите число 1"); 
+int num1=Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число 2"); 
+int num2=Convert.ToInt32(Console.ReadLine());
+
+if(num1 % num2==0) // если остаток от деления между числоми равен 0
+{
+    Console.WriteLine("да");
+}
+else
+{
+   Console.WriteLine("нет," + num1%num2); 
+}
